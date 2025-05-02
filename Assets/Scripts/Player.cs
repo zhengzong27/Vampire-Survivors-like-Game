5525dc2c06c20df6e48f50681f1df9da5fce99bd
zhengzong27 <131930290+zhengzong27@users.noreply.github.com>
2025-05-02 22:53:52 +0800

    导入

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+    public Scanner scanner;
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        scanner = GetComponent<Scanner>();
+    }
+
+private void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+    private void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+    private void LateUpdate()
+    {
+        anim.SetFloat("speed",inputVec.magnitude);
+        if(inputVec.x !=0) {
+            spriter.flipX = inputVec.x < 0;
+
+        }
+    }
+}
